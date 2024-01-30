64e9978891c7e38d4955d485359438ffc47308d1
jatinssh <jatin7838066@gmail.com>
2024-01-30 23:08:15 +0530

    updated

--- movie_search_website/src/App.jsx
+++ movie_search_website/src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
 import './App.css'
-
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
+     <div className="container">
+      <header>
+        <h2>Movie Search</h2>
+        <input type='text' className='inputsearch' placeholder='Search the movie here..'></input>
+      </header>
+     </div>
+     <div>
+      
+     </div>
        
     </>
   )
